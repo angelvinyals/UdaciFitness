23ad9a089eb4d25293d3790b3863b6413f38c4a1
angelvinyals <angel.vinyals@gmail.com>
2018-07-08 00:31:38 +0200

    Create UdaciSlider component
    add content to UdaciSlider importing slider and passing props

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
-import { Icon } from 'react-native-elements'
-import { Ionicons } from '@expo/vector-icons'
+
 
 
 export default class App extends React.Component {
   render() {
     return (
       <View >
-        <AddEntry/>     
+        <AddEntry />
       </View>
     )
   }
 }
+
